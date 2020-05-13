cc9e45c6e41e5c79d4161b3c7a32dc57ca429133
Renato Groffe <renato.groffe@yahoo.com.br>
2020-05-13 11:24:19 -0300

    GitHub Actions + AKS
    GitHub Actions + AKS

---
+++ Controllers/ContadorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace APIContagem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContadorController : ControllerBase
+    {
+        private static Contador _CONTADOR = new Contador();
+
+        [HttpGet]
+        public object Get([FromServices]IConfiguration configuration)
+        {  
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+
+                if (_CONTADOR.ValorAtual % 3 == 0)
+                {
+                    throw new Exception("Testando erro");
+                }
+
+                return new
+                {
+                    _CONTADOR.ValorAtual,
+                    Environment.MachineName,
+                    Local = "Teste",
+                    Sistema = Environment.OSVersion.VersionString,
+                    Variavel = configuration["TesteAmbiente"],
+                    TargetFramework = Assembly
+                        .GetEntryAssembly()?
+                        .GetCustomAttribute<TargetFrameworkAttribute>()?
+                        .FrameworkName
+                };
+            }
+        }
+    }
+}
